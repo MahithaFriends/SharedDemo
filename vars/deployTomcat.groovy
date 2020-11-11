9108863d4c7721baa639403e387f9088933fa0fe
MahithaFriends <prasadocjp@gmail.com>
2020-11-11 11:25:55 +0530

    Create deployTomcat.groovy

---
+++ vars/deployTomcat.groovy
@@ -0,0 +1,8 @@
+def call(id,credId){
+sshagent([credId]){
+sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@${ip}:/opt/tomcat8/webapps/myweb.war"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/shutdown.sh"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
+  }
+  echo "Deploy to tomcat on ip ${ip}"
+}
